2a224bda96b8c5d95a1fa7d06e85302f10d83502
Boris <boris.mougoue@gmail.com>
2024-12-08 16:03:37 +0100

    vpc,subnet,VM and Firewall creation

--- 03-Terraform-Language-Basics/terraform-manifests/c1-versions.tf
+++ 03-Terraform-Language-Basics/terraform-manifests/c1-versions.tf
@@ -1,17 +1,20 @@
 # Terraform Settings Block
+# run the command ctrl + space to see the possible top level blocks
 terraform {
-  required_version = ">= 1.8.5"
+  required_version = ">=1.8.5"
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = ">= 5.33.0"
+      version = ">=6.12.0"
     }
   }
-}
+  }
+
 
 # Terraform Provider Block
 provider "google" {
-  project = "gcplearn9" # PROJECT_ID
+  project = "fluid-axis-444106-d7" # PROJECT_ID
   region = "us-central1"
+  
 }
 
--- 03-Terraform-Language-Basics/terraform-manifests/c5-vminstance.tf
+++ 03-Terraform-Language-Basics/terraform-manifests/c5-vminstance.tf
@@ -3,16 +3,19 @@ resource "google_compute_instance" "myapp1" {
   name         = "myapp1"
   machine_type = "e2-micro"
   zone         = "us-central1-a"
-  tags        = [tolist(google_compute_firewall.fw_ssh.target_tags)[0], tolist(google_compute_firewall.fw_http.target_tags)[0]]
+  # you can directly reference the tags = ["ssh-tag","webser-tag"]
+  # but the best practice is to get the value from the firewall resource using the tolist function
+  tags         = [tolist(google_compute_firewall.fw_ssh.target_tags)[0], tolist(google_compute_firewall.fw_http.target_tags)[0]]
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-12"
     }
   }
-  # Install Webserver
+  # Install Webserver; path.module means that the script is your current working dir
   metadata_startup_script = file("${path.module}/app1-webserver-install.sh")
   network_interface {
-    subnetwork = google_compute_subnetwork.mysubnet.id 
+    
+    subnetwork = google_compute_subnetwork.mysubnet.id
     access_config {
       # Include this section to give the VM an external IP address
     }
