fe9998159d00d4030a1d9f06b83e046b9da5af39
Boris <boris.mougoue@gmail.com>
2024-12-13 19:39:20 +0100

    HPA for private standard GKE Cluster

--- 13-GKE-Horizontal-Pod-Autoscaling/p4-k8sresources-terraform-manifests/c6-kubernetes-hpa.tf
+++ 13-GKE-Horizontal-Pod-Autoscaling/p4-k8sresources-terraform-manifests/c6-kubernetes-hpa.tf
@@ -7,11 +7,11 @@ resource "kubernetes_horizontal_pod_autoscaler_v2" "cpu_autoscaler" {
   spec {
     min_replicas = 1
     max_replicas = 10
-
+   # we are enabling HPA for the deployment resource
     scale_target_ref {
       api_version = "apps/v1"
       kind        = "Deployment"
-      name        = kubernetes_deployment_v1.myapp1.metadata[0].name 
+      name        = kubernetes_deployment_v1.myapp1.metadata[0].name # [0] refers to the first block
     }
     metric {
       type = "Resource"
